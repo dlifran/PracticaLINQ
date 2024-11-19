6ab8132953d276fdffe57c5f912cfeb5f9dd1abc
dlifran <darioifran@hotmail.com>
2024-11-19 16:42:13 -0300

    se agrego lógica para numeros impares

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ namespace PracticaLINQ
         {
             List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var numerosPares = numeros.Where(n => n % 2 == 0).ToList();
+            var numerosImpares = numeros.Where(n => n % 2 != 0).ToList();
             Console.WriteLine(string.Join(", ", numerosPares));
+            Console.WriteLine(string.Join(", ", numerosImpares));
         }
     }
 }
