f11b61bd99e49b546b887357ddeca766d1087f4b
dlifran <darioifran@hotmail.com>
2024-11-19 16:19:24 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticaLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var numerosPares = numeros.Where(n => n % 2 == 0).ToList();
+            Console.WriteLine(string.Join(", ", numerosPares));
+        }
+    }
+}
+
